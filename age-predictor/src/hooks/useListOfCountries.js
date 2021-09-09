3c81afc4dd3e149f26a316fd439bdfb2f97ddab1
Tomás <tomas950923@gmail.com>
2021-09-09 12:41:03 -0500

    added custom hook countries

---
+++ age-predictor/src/hooks/useListOfCountries.js
@@ -0,0 +1,30 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const useListOfCountries = ({ countryCode }) => {
+  const [countries, setCountries] = useState(null);
+  useEffect(() => {
+    const fetchCountries = (code) => {
+      if (code !== undefined)
+        // Si el countryCode esta especificado retorna el pais referente al codigo.
+        return axios({ method: "GET", url: "" })
+          .then((response) => setCountries(response.data))
+          .catch((error) => {
+            console.log(error);
+            setCountries(null);
+          });
+      // Si el countryCode no esta especificado retorna todos los paises.
+      return axios({ method: "GET", url: "" })
+        .then((response) => setCountries(response.data))
+        .catch((error) => {
+          console.log(error);
+          setCountries(null);
+        });
+    };
+    fetchCountries(countryCode);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+  return countries;
+};
+
+export default useListOfCountries;
